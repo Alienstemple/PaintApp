8a4098873a8957bdba45eccd912d314aaae90322
Alyona <borisovsk_a@mail.ru>
2023-01-10 16:12:35 +0300

    refactor: ColorInt annotation

--- app/src/main/java/com/example/paintapp/FigureFactory.kt
+++ app/src/main/java/com/example/paintapp/FigureFactory.kt
@@ -2,9 +2,10 @@ package com.example.paintapp
 
 import android.graphics.Path
 import android.graphics.PointF
+import androidx.annotation.ColorInt
 
 class FigureFactory {
-    fun createFigure(figureType: FigureType, currentColor: Int, currentPoint: PointF): AbstractFigure = when (figureType) {
+    fun createFigure(figureType: FigureType, @ColorInt currentColor: Int, currentPoint: PointF): AbstractFigure = when (figureType) {
         FigureType.RECT -> BoxFigure(currentColor)
         FigureType.LINE -> LineFigure(Path(), currentColor)
         FigureType.STRAIGHT -> StraightFigure(currentPoint, currentPoint, currentColor)
